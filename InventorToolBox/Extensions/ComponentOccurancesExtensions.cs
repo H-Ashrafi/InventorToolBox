a2339a8d537e4e713023e35732aba566694ecf02
Hymma.net <info@hymma.net>
2021-08-31 16:19:46 +1000

    bugs

--- InventorToolBox/Extensions/ComponentOccurancesExtensions.cs
+++ InventorToolBox/Extensions/ComponentOccurancesExtensions.cs
@@ -8,48 +8,6 @@ namespace InventorToolBox
     /// </summary>
     public static class ComponentOccurancesExtension
     {
-        #region private fields
-
-        private static List<ComponentOccurrence> _list = new List<ComponentOccurrence>();
-        #endregion
-
-        #region private methode/fuctions
-
-        /// <summary>
-        /// recursively processes a document and adds componets to a private filed <see cref="_list"/>
-        /// </summary>
-        /// <param name="componentOccurrences"></param>
-        /// <param name="targetDoc"></param>
-        private static void CalculateAllNonPhantomNonReferencedOccurances(ComponentOccurrences componentOccurrences, object targetDoc)
-        {
-            foreach (ComponentOccurrence occurrence in componentOccurrences)
-            {
-                if (occurrence.Definition.BOMStructure != BOMStructureEnum.kReferenceBOMStructure
-                   &&
-                   occurrence.Definition.BOMStructure != BOMStructureEnum.kPhantomBOMStructure)
-                {
-                    if (occurrence.Definition.Document == targetDoc)
-                    {
-                        _list.Add(occurrence);
-                    }
-                    else if (occurrence.DefinitionDocumentType == DocumentTypeEnum.kAssemblyDocumentObject)
-                    {
-                        CalculateAllNonPhantomNonReferencedOccurances(occurrence.Definition.Occurrences, targetDoc);
-                    }
-                }
-            }
-        }
-        #endregion
-
-        /// <summary>
-        /// list of occuraces that are not phantom nor are set as referenced in their document settings.
-        /// </summary>
-        /// <param name="targetDoc">the document that needs to be searched for</param>
-        /// <returns>List<ComponentOccurrence></returns>
-        public static List<ComponentOccurrence> AllNonPhantomNonReferencedOccurances(this ComponentOccurrences componentOccurrences, object targetDoc)
-        {
-            CalculateAllNonPhantomNonReferencedOccurances(componentOccurrences, targetDoc);
-            return _list;
-        }
+        
     }
 }
